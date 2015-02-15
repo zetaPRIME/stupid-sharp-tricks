e9712c0a592d4f087ca5806a0a7d2a537ee907c1
zetaPRIME <qmanxt@gmail.com>
2015-02-15 03:07:23 -0500

    lesson 1: accidental modification of runtime state during a breakpoint

---
+++ stupid-sharp-tricks/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using StupidSharpTricks.PropertyAbuse;
+
+namespace StupidSharpTricks
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			PMessage pm = PMessage.New["test1", 32]["test2", 24].IncrementAll;
+
+			breakpt();
+		}
+
+
+		static void breakpt()
+		{
+			// don't compile away
+			bool blah = false;
+			blah = !blah;
+		}
+	}
+}
---
+++ stupid-sharp-tricks/Property Abuse/PMessage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StupidSharpTricks.PropertyAbuse
+{
+	public class PMessage
+	{
+		// enforce more "concise" syntax
+		private PMessage() { }
+		public static PMessage New { get { return new PMessage(); } }
+
+		// data structures
+		public Dictionary<string, float> floats = new Dictionary<string, float>();
+
+		public PMessage this[string key, float value]
+		{
+			get
+			{
+				floats.Add(key, value);
+				return this;
+			}
+		}
+
+		// fun fact, this is of course triggered by mousing over the object in the IDE... so you can modify runtime state from within a breakpoint! @.@
+		public PMessage IncrementAll
+		{
+			get
+			{
+				List<string> keys = new List<string>();
+				foreach (KeyValuePair<string, float> kvp in floats) keys.Add(kvp.Key);
+				foreach (string k in keys) floats[k]++;
+				return this;
+			}
+		}
+	}
+}
